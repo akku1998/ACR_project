da470191569a2de5cce677be3e1c776311b49f5d
Mohanty <Akanksha.Mohanty@ext.saint-gobain.com>
2020-02-24 18:36:41 +0530

    fourth commit

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var sass = require('gulp-sass');
 
 // Task to compile scss to css
 gulp.task('sass', function(){
-  return gulp.src('app/scss/page_three.scss')
+  return gulp.src('app/scss/styles.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(gulp.dest('app/css'))
 });
